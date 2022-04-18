16e800762baa8b904db171387f9ec50d9df0226d
vrajjj07 <103171727+vrajjj07@users.noreply.github.com>
2022-04-18 13:01:13 +0530

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#hidden").hover(function(){
+        $(this).css("color","black")
+    },
+    function(){
+        $(this).hide();
+        $(this).css("display","none");
+    }); 
+
+});
